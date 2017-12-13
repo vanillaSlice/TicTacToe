a65cbdf2ba533abc781b60f122958319c321f1ce
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-12-13 16:34:28 +0000

    Finished tidy up

--- main.js
+++ main.js
@@ -5,52 +5,59 @@ window.addEventListener('load', function () {
   /*
    * DOM elements.
    */
-  var modeInputElements = document.querySelectorAll('.mode-btns input'),
-    playerElements = document.querySelector('.players'),
-    playerXElement = playerElements.children[0],
-    playerXScoreElement = playerXElement.children[1],
-    playerOElement = playerElements.children[1],
-    playerOScoreElement = playerOElement.children[1],
-    messageElement = document.querySelector('.message'),
-    gridElement = document.querySelector('.grid'),
-    maskElement = document.querySelector('.mask'),
-    cellElements = document.querySelectorAll('.cell'),
-    restartButtonElement = document.querySelector('.restart-btn');
+  var modeInputElements = document.querySelectorAll('.mode-btns .radio');
+  var playersElement = document.querySelector('.players');
+  var playerXElement = document.querySelector('.player-x');
+  var playerXScoreElement = document.querySelector('.player-x .score');
+  var playerOElement = document.querySelector('.player-o');
+  var playerOScoreElement = document.querySelector('.player-o .score');
+  var messageElement = document.querySelector('.message');
+  var gridElement = document.querySelector('.grid');
+  var maskElement = document.querySelector('.grid .mask');
+  var cellElements = document.querySelectorAll('.grid .cell');
+  var restartButtonElement = document.querySelector('.restart-btn');
 
   /*
    * SVG templates.
    */
-  var crossTemplate = '<svg width="100" height="100">' +
-    '<path class="cross-one" stroke="#545454" stroke-width="8" fill="none"' +
-    ' d="M20,20L80,80" />' +
-    '<path class="cross-two" stroke="#545454" stroke-width="8" fill="none"' +
-    ' d="M80,20L20,80" />' +
-    '</svg>',
-    circleTemplate = '<svg width="100" height="100">' +
-    '<path class="circle" stroke="#f2ebd3" stroke-width="8" fill="none"' +
-    ' d="M20,50a30,30 0 1,0 60,0a30,30 0 1,0 -60,0" />' +
+  var crossTemplate =
+    '<svg width="100" height="100">' +
+    '<path class="cross-one" stroke="#545454" stroke-width="8" fill="none" d="M20,20L80,80" />' +
+    '<path class="cross-two" stroke="#545454" stroke-width="8" fill="none" d="M80,20L20,80" />' +
     '</svg>';
+  var circleTemplate =
+    '<svg width="100" height="100">' +
+    '<path class="circle" stroke="#f2ebd3" stroke-width="8" fill="none" d="M20,50a30,30 0 1,0 60,0a30,30 0 1,0 -60,0" />' +
+    '</svg>';
+
+  /*
+   * Constants.
+   */
+  var winningLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
 
   /*
    * Variables.
    */
-  var mode = 'medium',
-    playerX = Player('x'),
-    playerO = Player('o'),
-    playerWithTurn,
-    grid,
-    winningLines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ],
-    hasStarted,
-    isGameOver;
+  var mode = 'medium';
+  var playerX = Player('x');
+  var playerO = Player('o');
+  var playerWithTurn;
+  var grid;
+  var hasStarted;
+  var isGameOver;
+
+  /*
+   * Helpers. 
+   */
 
   function Player(symbol) {
     return {
@@ -85,7 +92,7 @@ window.addEventListener('load', function () {
 
     // replace player x element so border animation plays
     var newPlayerXElement = playerXElement.cloneNode(true);
-    playerElements.replaceChild(newPlayerXElement, playerXElement);
+    playersElement.replaceChild(newPlayerXElement, playerXElement);
     playerXElement = newPlayerXElement;
     playerXScoreElement = playerXElement.children[1];
 
@@ -105,16 +112,20 @@ window.addEventListener('load', function () {
       startGame();
     } else {
       playerO.type = 'computer';
-      messageElement.innerHTML = '<p class="text">Start game or select player</p>';
+      setMessage('Start game or select player');
     }
   }
 
   function startGame() {
     hasStarted = true;
-    messageElement.innerHTML = '<p class="text">x turn</p>';
+    setMessage(playerWithTurn.symbol + ' turn');
     if (playerWithTurn.type === 'computer') {
       computerMakeMove();
     }
+  }
+
+  function setMessage(message) {
+    messageElement.innerHTML = '<p class="text">' + message + '</p>';
   }
 
   function computerMakeMove() {
@@ -144,25 +155,28 @@ window.addEventListener('load', function () {
     var bestMove = [-2];
     var symbol = playerWithTurn.symbol;
     grid.forEach(function (element, index) {
-      if (element === '') {
-        var val = minimax(index, symbol, symbol);
-        if (bestMove[0] < val) {
-          bestMove[0] = val;
-          bestMove[1] = index;
-        }
+      // can't make move in cell that's already filled
+      if (element !== '') {
+        return;
+      }
+
+      var val = minimax(index, symbol, symbol);
+      if (bestMove[0] < val) {
+        bestMove[0] = val;
+        bestMove[1] = index;
       }
     });
     return bestMove[1];
   }
 
   function minimax(index, symbol, computerSymbol) {
-    var points,
-      nextSymbol;
+    var points;
+    var nextSymbol;
 
     // set the symbol
     grid[index] = symbol;
 
-    if (isGameWon(symbol)) {
+    if (isWinner(symbol)) {
       points = (symbol === computerSymbol) ? 1 : -1;
     } else if (isGridFilled()) {
       points = 0;
@@ -187,32 +201,25 @@ window.addEventListener('load', function () {
       }
     }
 
-    // clean up
+    // clear the symbol
     grid[index] = '';
 
     return points;
   }
 
-  function isGameWon(symbol) {
-    for (var i = 0, length = winningLines.length; i < length; i++) {
-      var line = winningLines[i],
-        first = grid[line[0]],
-        second = grid[line[1]],
-        third = grid[line[2]];
-      if (first === symbol && first === second && second === third) {
-        return true;
-      }
-    }
-    return false;
+  function isWinner(symbol) {
+    return winningLines.some(function (line) {
+      var first = grid[line[0]];
+      var second = grid[line[1]];
+      var third = grid[line[2]];
+      return symbol === first && first === second && second === third;
+    });
   }
 
   function isGridFilled() {
-    for (var i = 0, length = grid.length; i < length; i++) {
-      if (grid[i] === '') {
-        return false;
-      }
-    }
-    return true;
+    return grid.every(function (e) {
+      return e !== '';
+    });
   }
 
   function makeMove(index) {
@@ -227,20 +234,20 @@ window.addEventListener('load', function () {
     cellElements[index].innerHTML = (symbol === 'o') ? circleTemplate : crossTemplate;
 
     // 3. Check if game is over, if not switch players
-    if (isGameWon(playerWithTurn.symbol)) {
+    if (isWinner(playerWithTurn.symbol)) {
       playerWithTurn.score++;
       playerXScoreElement.innerText = (playerX.score === 0) ? '-' : playerX.score;
       playerOScoreElement.innerText = (playerO.score === 0) ? '-' : playerO.score;
-      messageElement.innerHTML = '<p class="text">' + symbol + ' wins</p>';
+      setMessage(symbol + ' wins');
       isGameOver = true;
     } else if (isGridFilled()) {
-      messageElement.innerHTML = '<p class="text">Draw</p>';
+      setMessage('Draw');
       isGameOver = true;
     } else {
       playerWithTurn = (playerWithTurn === playerX) ? playerO : playerX;
       playerXElement.classList.toggle('has-turn');
       playerOElement.classList.toggle('has-turn');
-      messageElement.innerHTML = '<p class="text">' + playerWithTurn.symbol + ' turn</p>';
+      setMessage(playerWithTurn.symbol + ' turn');
       if (playerWithTurn.type === 'computer') {
         computerMakeMove();
       }
