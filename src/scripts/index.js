ac091bfd2bba638c4a80e36f469ec745e23520fd
Mike Lowe <mikelowedev@gmail.com>
2019-02-16 12:16:53 +0000

    Refactoring

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -63,7 +63,7 @@ const playerO = new Player('o');
 
 let mode = 'medium';
 let playerWithTurn;
-let grid;
+const grid = Array(9);
 let hasStarted;
 let isGameOver;
 
@@ -71,8 +71,146 @@ let isGameOver;
  * Functions
  */
 
+function handleModeChange(e) {
+  mode = e.target.value;
+  resetGame();
+}
+
+function resetGame() {
+  resetState();
+  resetDomElements();
+
+  if (mode === 'multiplayer') {
+    startMultiplayerGame();
+  } else {
+    setupGameAgainstComputer();
+  }
+}
+
+function resetState() {
+  playerX.type = 'human';
+  playerWithTurn = playerX;
+  grid.fill('');
+  hasStarted = false;
+  isGameOver = false;
+}
+
+function resetDomElements() {
+  highlightPlayerElementWithTurn();
+  replacePlayerXElement();
+  clearCellElements();
+  replaceGridMaskElement();
+}
+
+function highlightPlayerElementWithTurn() {
+  playerXElement.classList.toggle('has-turn', playerWithTurn === playerX);
+  playerOElement.classList.toggle('has-turn', playerWithTurn === playerO);
+}
+
+// Do this so the border animation plays
+function replacePlayerXElement() {
+  const newPlayerXElement = playerXElement.cloneNode(true);
+  playersElement.replaceChild(newPlayerXElement, playerXElement);
+  playerXElement = newPlayerXElement;
+  [, playerXScoreElement] = playerXElement.children;
+}
+
+function clearCellElements() {
+  cellElements.forEach((e) => {
+    e.innerHTML = '';
+  });
+}
+
+// Do this so the grid animation plays
+function replaceGridMaskElement() {
+  const newGridMaskElement = gridMaskElement.cloneNode(true);
+  gridElement.replaceChild(newGridMaskElement, gridMaskElement);
+  gridMaskElement = newGridMaskElement;
+}
+
+function startMultiplayerGame() {
+  playerO.type = 'human';
+  startGame();
+}
+
+function startGame() {
+  hasStarted = true;
+  setMessage(`${playerWithTurn.symbol} turn`);
+  if (playerWithTurn.type === 'computer') {
+    computerMakeMove();
+  }
+}
+
 function setMessage(message) {
   messageElement.innerHTML = `<p class="text">${message}</p>`;
+}
+
+function computerMakeMove() {
+  let index = findBestMoveIndex();
+
+  if (mode === 'easy' || mode === 'medium') {
+    const emptyCells = grid.reduce((acc, cell, i) => {
+      if (cell === '') {
+        acc.push(i);
+      }
+      return acc;
+    }, []);
+
+    const threshold = mode === 'easy' ? 0.1 : 0.9;
+    if (Math.random() > threshold) {
+      emptyCells.splice(emptyCells.indexOf(index), 1);
+      index = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+    }
+  }
+
+  setTimeout(() => makeMove(index), 1000);
+}
+
+function findBestMoveIndex() {
+  const bestMove = { points: -2 };
+  const { symbol } = playerWithTurn;
+
+  grid.forEach((_, index) => {
+    if (grid[index] !== '') {
+      return;
+    }
+
+    const points = minimax(index, symbol, symbol);
+
+    if (bestMove.points < points) {
+      bestMove.points = points;
+      bestMove.index = index;
+    }
+  });
+
+  return bestMove.index;
+}
+
+function minimax(index, symbol, computerSymbol) {
+  grid[index] = symbol;
+
+  if (isWinner(symbol)) {
+    grid[index] = '';
+    return symbol === computerSymbol ? 1 : -1;
+  }
+
+  if (isGridFilled()) {
+    grid[index] = '';
+    return 0;
+  }
+
+  let points;
+  const nextSymbol = symbol === 'o' ? 'x' : 'o';
+
+  if (nextSymbol === computerSymbol) {
+    points = grid.reduce((max, cur, i) => (cur === '' ? Math.max(max, minimax(i, nextSymbol, computerSymbol)) : max), -2);
+  } else {
+    points = grid.reduce((min, cur, i) => (cur === '' ? Math.min(min, minimax(i, nextSymbol, computerSymbol)) : min), 2);
+  }
+
+  grid[index] = '';
+
+  return points;
 }
 
 function isWinner(symbol) {
@@ -88,122 +226,6 @@ function isGridFilled() {
   return grid.every(e => e !== '');
 }
 
-function getCellSymbol(index) {
-  return grid[index];
-}
-
-function setCellSymbol(index, symbol) {
-  grid[index] = symbol;
-}
-
-function drawCellSymbol(index) {
-  cellElements[index].innerHTML = getCellSymbol(index) === 'o' ? circleTemplate : crossTemplate;
-}
-
-function clearCellSymbol(index) {
-  grid[index] = '';
-}
-
-function isCellOccupied(index) {
-  return grid[index] !== '';
-}
-
-function resetState() {
-  playerX.type = 'human';
-  playerWithTurn = playerX;
-  grid = ['', '', '', '', '', '', '', '', ''];
-  hasStarted = false;
-  isGameOver = false;
-}
-
-function highlightPlayerElementWithTurn() {
-  if (playerWithTurn === playerX) {
-    playerXElement.classList.add('has-turn');
-    playerOElement.classList.remove('has-turn');
-  } else {
-    playerXElement.classList.remove('has-turn');
-    playerOElement.classList.add('has-turn');
-  }
-}
-
-// we do this so the border animation plays
-function replacePlayerXElement() {
-  const newPlayerXElement = playerXElement.cloneNode(true);
-  playersElement.replaceChild(newPlayerXElement, playerXElement);
-  playerXElement = newPlayerXElement;
-  [, playerXScoreElement] = playerXElement.children;
-}
-
-function clearCellElements() {
-  cellElements.forEach((e) => {
-    e.innerHTML = '';
-  });
-}
-
-// we do this so the grid animation plays
-function replaceGridMaskElement() {
-  const newGridMaskElement = gridMaskElement.cloneNode(true);
-  gridElement.replaceChild(newGridMaskElement, gridMaskElement);
-  gridMaskElement = newGridMaskElement;
-}
-
-function resetDomElements() {
-  highlightPlayerElementWithTurn();
-  replacePlayerXElement();
-  clearCellElements();
-  replaceGridMaskElement();
-}
-
-function minimax(index, symbol, computerSymbol) {
-  setCellSymbol(index, symbol);
-
-  if (isWinner(symbol)) {
-    clearCellSymbol(index);
-    return symbol === computerSymbol ? 1 : -1;
-  }
-
-  if (isGridFilled()) {
-    clearCellSymbol(index);
-    return 0;
-  }
-
-  let points;
-  const nextSymbol = symbol === 'o' ? 'x' : 'o';
-
-  if (nextSymbol === computerSymbol) {
-    points = grid.reduce((max, cur, i) => (cur === '' ? Math.max(max, minimax(i, nextSymbol, computerSymbol)) : max), -2);
-  } else {
-    points = grid.reduce((min, cur, i) => (cur === '' ? Math.min(min, minimax(i, nextSymbol, computerSymbol)) : min), 2);
-  }
-
-  clearCellSymbol(index);
-
-  return points;
-}
-
-function findBestMoveIndex() {
-  const bestMove = { points: -2 };
-  const { symbol } = playerWithTurn;
-
-  grid.forEach((_, index) => {
-    if (isCellOccupied(index)) {
-      return;
-    }
-
-    const points = minimax(index, symbol, symbol);
-    if (bestMove.points < points) {
-      bestMove.points = points;
-      bestMove.index = index;
-    }
-  });
-
-  return bestMove.index;
-}
-
-function isMoveAllowed(index) {
-  return hasStarted && !isGameOver && grid[index] === '';
-}
-
 function makeMove(index) {
   if (!isMoveAllowed(index)) {
     return;
@@ -211,77 +233,40 @@ function makeMove(index) {
 
   const { symbol } = playerWithTurn;
 
-  setCellSymbol(index, symbol);
-  drawCellSymbol(index);
+  grid[index] = symbol;
+  cellElements[index].innerHTML = grid[index] === 'o' ? circleTemplate : crossTemplate;
 
-  // 3. Check if game is over, if not switch players
   if (isWinner(playerWithTurn.symbol)) {
     playerWithTurn.score += 1;
     playerXScoreElement.innerText = (playerX.score === 0) ? '-' : playerX.score;
     playerOScoreElement.innerText = (playerO.score === 0) ? '-' : playerO.score;
     setMessage(`${symbol} wins`);
     isGameOver = true;
-  } else if (isGridFilled()) {
+    return;
+  }
+
+  if (isGridFilled()) {
     setMessage('Draw');
     isGameOver = true;
-  } else {
-    playerWithTurn = (playerWithTurn === playerX) ? playerO : playerX;
-    highlightPlayerElementWithTurn();
-    setMessage(`${playerWithTurn.symbol} turn`);
-    if (playerWithTurn.type === 'computer') {
-      computerMakeMove();
-    }
-  }
-}
-
-function computerMakeMove() {
-  let index = findBestMoveIndex();
-
-  if (mode === 'easy' || mode === 'medium') {
-    const emptyCells = grid.reduce((acc, current, i) => {
-      if (current === '') {
-        acc.push(i);
-      }
-      return acc;
-    }, []);
-
-    const threshold = (mode === 'easy') ? 0.1 : 0.9;
-    if (Math.random() > threshold) {
-      emptyCells.splice(emptyCells.indexOf(index), 1);
-      index = emptyCells[Math.floor(Math.random() * emptyCells.length)];
-    }
+    return;
   }
 
-  setTimeout(() => {
-    makeMove(index);
-  }, 1000);
-}
-
-function startGame() {
-  hasStarted = true;
+  playerWithTurn = (playerWithTurn === playerX) ? playerO : playerX;
+  highlightPlayerElementWithTurn();
   setMessage(`${playerWithTurn.symbol} turn`);
+
   if (playerWithTurn.type === 'computer') {
     computerMakeMove();
   }
 }
 
-function resetGame() {
-  resetState();
-  resetDomElements();
-
-  // 3. Start game automatically if multiplayer, otherwise wait
-  if (mode === 'multiplayer') {
-    playerO.type = 'human';
-    startGame();
-  } else {
-    playerO.type = 'computer';
-    setMessage('Start game or select player');
-  }
+function isMoveAllowed(index) {
+  return hasStarted && !isGameOver && grid[index] === '';
 }
 
-function handleModeChange(e) {
-  mode = e.target.value;
-  resetGame();
+function setupGameAgainstComputer() {
+  playerO.type = 'computer';
+  setMessage('Start game or select player');
 }
 
 function handlePlayerOElementClick() {
